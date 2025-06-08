6ef9b65d8da5680e19c957931946ffd6b91af7f7
Niudfear <hello@itcast.cn>
2025-06-08 16:13:54 +0800

    feat: 成长积分

--- coursedesign-B22042118/src/main/java/com/course/service/impl/BfzNoteServiceImpl.java
+++ coursedesign-B22042118/src/main/java/com/course/service/impl/BfzNoteServiceImpl.java
@@ -1,13 +1,32 @@
 package com.course.service.impl;
 
+import com.course.common.utils.FileUtils;
+import com.course.common.utils.JsonUtils;
+import com.course.entity.bo.PointObject;
 import com.course.service.BfzNoteService;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+
 /**
  * 并发症记录
  */
 @Service
 public class BfzNoteServiceImpl implements BfzNoteService {
+    @Override
     public void bfzNote() {
-        System.out.println("+++++bfzNote积分计算方法执行+++++");
+        String file = FileUtils.readFile("score");
+        PointObject pointObject = JsonUtils.jsonToPojo(file, PointObject.class);
+        Calendar calendar = Calendar.getInstance();
+        int currentYear = calendar.get(Calendar.YEAR);
+        if (pointObject.getLastBfzNoteYear() == null || pointObject.getLastBfzNoteYear() != currentYear) {
+            pointObject.setGrowScore(pointObject.getGrowScore() + 3);
+            pointObject.setScoreTotal(pointObject.getScoreTotal() + 3);
+            pointObject.setLastBfzNoteYear(currentYear);
+            String content = JsonUtils.objectToJson(pointObject);
+            FileUtils.writeFile("score", content);
+            System.out.println("+++++bfzNote积分计算方法执行+++++");
+            System.out.println(pointObject);
+        }
     }
 }
--- coursedesign-B22042118/src/main/java/com/course/service/impl/LoginServiceImpl.java
+++ coursedesign-B22042118/src/main/java/com/course/service/impl/LoginServiceImpl.java
@@ -1,13 +1,37 @@
 package com.course.service.impl;
 
+import com.course.common.utils.FileUtils;
+import com.course.common.utils.JsonUtils;
+import com.course.entity.bo.PointObject;
 import com.course.service.LoginService;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * 登录平台
  */
 @Service
 public class LoginServiceImpl implements LoginService {
-    public void testDesign(){
-        System.out.println("+++++login积分计算方法执行+++++");
+    @Override
+    public void testDesign() {
+        String file = FileUtils.readFile("score");
+        PointObject pointObject = JsonUtils.jsonToPojo(file, PointObject.class);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        Date today = calendar.getTime();
+        if (pointObject.getLastLoginDate() == null ||!pointObject.getLastLoginDate().equals(today)) {
+            pointObject.setGrowScore(pointObject.getGrowScore() + 1);
+            pointObject.setScoreTotal(pointObject.getScoreTotal() + 1);
+            pointObject.setLastLoginDate(today);
+            String content = JsonUtils.objectToJson(pointObject);
+            FileUtils.writeFile("score", content);
+            System.out.println("+++++login积分计算方法执行+++++");
+            System.out.println(pointObject);
+        }
     }
 }
