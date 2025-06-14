e82716a5c304c37f57209a5f143e5bd828d394e8
Serein <2075337935@qq.com>
2025-06-14 22:31:41 +0800

    feat: implement score handling and rating services

--- coursedesign-B22042118/src/main/java/com/course/service/impl/ExtendedActivityServiceImpl.java
+++ coursedesign-B22042118/src/main/java/com/course/service/impl/ExtendedActivityServiceImpl.java
@@ -1,5 +1,8 @@
 package com.course.service.impl;
 
+import com.course.common.utils.FileUtils;
+import com.course.common.utils.JsonUtils;
+import com.course.entity.bo.PointObject;
 import com.course.service.ExtendedActivityService;
 import org.springframework.stereotype.Service;
 /**
@@ -7,8 +10,16 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ExtendedActivityServiceImpl implements ExtendedActivityService {
-    public void extendedActivity(){
+    @Override
+    public void extendedActivity() {
+        String file = FileUtils.readFile("score");
+        PointObject pointObject = JsonUtils.jsonToPojo(file, PointObject.class);
+        pointObject.setExchangeScore(pointObject.getExchangeScore() + 5);
+        pointObject.setScoreTotal(pointObject.getScoreTotal() + 5);
+        String content = JsonUtils.objectToJson(pointObject);
+        FileUtils.writeFile("score", content);
         System.out.println("+++++extendedActivity积分计算方法执行+++++");
-    }
+        System.out.println(pointObject);
 
+    }
 }
--- coursedesign-B22042118/src/main/java/com/course/service/impl/FollowUpServiceImpl.java
+++ coursedesign-B22042118/src/main/java/com/course/service/impl/FollowUpServiceImpl.java
@@ -1,5 +1,8 @@
 package com.course.service.impl;
 
+import com.course.common.utils.FileUtils;
+import com.course.common.utils.JsonUtils;
+import com.course.entity.bo.PointObject;
 import com.course.service.FollowUpService;
 import org.springframework.stereotype.Service;
 /**
@@ -7,7 +10,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowUpServiceImpl implements FollowUpService {
-    public void followUp(){
+    @Override
+    public void followUp() {
+        String file = FileUtils.readFile("score");
+        PointObject pointObject = JsonUtils.jsonToPojo(file, PointObject.class);
+        pointObject.setExchangeScore(pointObject.getExchangeScore() + 3);
+        pointObject.setScoreTotal(pointObject.getScoreTotal() + 3);
+        String content = JsonUtils.objectToJson(pointObject);
+        FileUtils.writeFile("score", content);
         System.out.println("+++++followUp积分计算方法执行+++++");
     }
 }
+
